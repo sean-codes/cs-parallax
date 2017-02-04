1f0de029d0b5b5c63ef99afdf6b65b275add4431
sean_codes <sean.mann@alpinehomeair.com>
2017-02-04 04:33:42 -0600

    [update] - full screen

--- js/index.js
+++ js/index.js
@@ -1,29 +1,28 @@
-var div = document.getElementsByClassName('container')[0];
-var scrollers = document.getElementsByClassName('scroller');
-div.onscroll = function(){
-   console.log('Checking scroll: ' + div.scrollTop);
-   checkScrollers();
-   
-}
+test = function(){
+    var div = document.body;
+    var scrollers = document.getElementsByClassName('scroller');
+    div.onscroll = function() {
+        checkScrollers();
+    }
 
-checkScrollers = function(){
-   var y = 0;
-   for(var i = 0; i < scrollers.length; i++){
-      y += scrollers[i].offsetHeight;
-      bg = scrollers[i].getElementsByClassName('parallax')[0];
-      console.log(i+1);
-      bg2 = scrollers[i+1].getElementsByClassName('parallax')[0];
-      if(div.scrollTop < y)
-         break;
-   }
-   var percent = Math.floor((1-(y - div.scrollTop)/scrollers[i].offsetHeight) * 100);
-   console.log('Percent of scroller: ' + i + ' - ' + percent);
-   
-   bg.style.transform = 'translateY(-' + percent + 'px)';
-   bg.style.marginBottom = -percent + 'px';
-   
-   if(bg2){
-      bg2.style.transform = 'translateY(0)';
-      bg2.style.marginBottom = '0px';
-   }
+    checkScrollers = function() {
+        var y = 0;
+        for (var i = 0; i < scrollers.length; i++) {
+            y += scrollers[i].offsetHeight;
+            bg = scrollers[i].getElementsByClassName('parallax')[0];
+            if (scrollers[i + 1])
+                bg2 = scrollers[i + 1].getElementsByClassName('parallax')[0];
+            if (div.scrollTop < y)
+                break;
+        }
+        var percent = Math.floor((1 - (y - div.scrollTop) / scrollers[i].offsetHeight) * 100);
+
+        bg.style.transform = 'translateY(-' + percent*2 + 'px)';
+        bg.style.marginBottom = -percent*2 + 'px';
+
+        if (bg2) {
+            bg2.style.transform = 'translateY(0)';
+            bg2.style.marginBottom = '0px';
+        }
+    }
 }
