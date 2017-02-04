efd93ae02885a40e42da38e9fa09c3508eddd0ef
sean_codes <sean.mann@alpinehomeair.com>
2017-02-04 04:37:16 -0600

    [update] - logs

--- js/index.js
+++ js/index.js
@@ -4,18 +4,20 @@ test = function(){
     div.onscroll = function() {
         checkScrollers();
     }
-
+    console.log('Detected Scrollers: ' + scrollers);
     checkScrollers = function() {
         var y = 0;
         for (var i = 0; i < scrollers.length; i++) {
             y += scrollers[i].offsetHeight;
             bg = scrollers[i].getElementsByClassName('parallax')[0];
+            console.log(i + 1);
             if (scrollers[i + 1])
                 bg2 = scrollers[i + 1].getElementsByClassName('parallax')[0];
             if (div.scrollTop < y)
                 break;
         }
         var percent = Math.floor((1 - (y - div.scrollTop) / scrollers[i].offsetHeight) * 100);
+        console.log('Percent of scroller: ' + i + ' - ' + percent);
 
         bg.style.transform = 'translateY(-' + percent*2 + 'px)';
         bg.style.marginBottom = -percent*2 + 'px';
