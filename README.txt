f635ae0bc0dfeb9d03aacb954ea7204476544083
sean_codes <sean.mann@alpinehomeair.com>
2017-02-04 03:52:22 -0600

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/sean_codes/pen/PWeyGP.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,29 @@
+var div = document.getElementsByClassName('container')[0];
+var scrollers = document.getElementsByClassName('scroller');
+div.onscroll = function(){
+   console.log('Checking scroll: ' + div.scrollTop);
+   checkScrollers();
+   
+}
+
+checkScrollers = function(){
+   var y = 0;
+   for(var i = 0; i < scrollers.length; i++){
+      y += scrollers[i].offsetHeight;
+      bg = scrollers[i].getElementsByClassName('parallax')[0];
+      console.log(i+1);
+      bg2 = scrollers[i+1].getElementsByClassName('parallax')[0];
+      if(div.scrollTop < y)
+         break;
+   }
+   var percent = Math.floor((1-(y - div.scrollTop)/scrollers[i].offsetHeight) * 100);
+   console.log('Percent of scroller: ' + i + ' - ' + percent);
+   
+   bg.style.transform = 'translateY(-' + percent + 'px)';
+   bg.style.marginBottom = -percent + 'px';
+   
+   if(bg2){
+      bg2.style.transform = 'translateY(0)';
+      bg2.style.marginBottom = '0px';
+   }
+}
